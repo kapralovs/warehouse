14ae6854b112890f2085e37e6df564789afb3d2e
kapralovs <ser-kapralov@yandex.ru>
2022-05-30 17:48:32 +0300

    WIP

--- internal/server/auth.go
+++ internal/server/auth.go
@@ -1,43 +1,43 @@
 package server
 
 import (
-	"encoding/base64"
 	"errors"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/kapralovs/warehouse/internal/data"
 	"github.com/kapralovs/warehouse/internal/users"
 )
 
-func checkCredentials(ds *data.DataStorage, username,password string) (*users.Profile, error) {
+func checkCredentials(ds *data.DataStorage, username, password string) bool {
 	/*
-	decodedCreds, err := base64.StdEncoding.DecodeString(encodedCreds)
-	if err != nil {
-		return nil, err
-	}
+		decodedCreds, err := base64.StdEncoding.DecodeString(encodedCreds)
+		if err != nil {
+			return nil, err
+		}
 
-	creds := strings.Split(string(decodedCreds), ":")
+		creds := strings.Split(string(decodedCreds), ":")
 	*/
 
 	for _, profile := range ds.Profiles {
 		if profile.Account.Username == username && profile.Account.Password == password {
-			log.Printf("Credentials \"%s\" are checked\n", encodedCreds)
-			return profile, nil
+			return true
 		}
 	}
 
-	return nil, errors.New("authorisation failed because credentials are incorrect")
+	return false
+	// errors.New("authorisation failed because credentials are incorrect")
 }
 
 func authorization(ds *data.DataStorage, w http.ResponseWriter, r *http.Request) (*users.Profile, error) {
 	/*
-	headerValue := r.Header.Get("Authorization")
-	encodedCreds := headerValue[len("Basic "):]
+		headerValue := r.Header.Get("Authorization")
+		encodedCreds := headerValue[len("Basic "):]
 	*/
-	username,password:=r.BasicAuth()
-	user, err := checkCredentials(ds, encodedCreds)
+	username, password, ok := r.BasicAuth()
+	if ok && checkCredentials(ds, username, password) {
+		return
+	}
 	if err != nil {
 		w.Header().Add("WWW-Authenticate", "Basic realm="+encodedCreds)
 		w.WriteHeader(http.StatusUnauthorized)
